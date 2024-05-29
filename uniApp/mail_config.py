15f1f17497fc66a20be85ac9006fa4a12f9b5896
Mustapha7018 <adamsmustapha419@gmail.com>
2024-05-29 01:46:24 +0000

    Add mail_config.py to .gitignore

--- uniApp/mail_config.py
+++
@@ -1,7 +0,0 @@
-# Email Configuration
-EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_PORT=587
-EMAIL_USE_TLS=True
-EMAIL_HOST_USER='uniapp135@gmail.com'
-EMAIL_HOST_PASSWORD='ttiisksrpcslurjo'
