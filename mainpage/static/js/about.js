2cbe3a1b6891acc02da1e12f4a4dad2102dff1f1
Mustapha7018 <adamsmustapha419@gmail.com>
2024-09-21 21:14:28 +0000

    midify search page hero css

--- mainpage/static/js/about.js
+++ mainpage/static/js/about.js
@@ -22,3 +22,49 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
+
+  // Dropdown
+  document.addEventListener('DOMContentLoaded', function() {
+    const dropbtn = document.querySelector('.dropbtn');
+    const dropdownItems = document.querySelectorAll('.dropdown-content a');
+    let currentSelection = 'Public'; 
+  
+    dropbtn.textContent = currentSelection; 
+  
+    dropdownItems.forEach(item => {
+        item.addEventListener('click', function(e) {
+  
+            const selectedText = e.target.textContent;
+            dropbtn.textContent = selectedText;
+            currentSelection = selectedText;
+  
+            updateDropdownList(selectedText);
+        });
+    });
+  
+    function updateDropdownList(selectedText) {
+        const dropdownContent = document.querySelector('.dropdown-content');
+        while (dropdownContent.firstChild) {
+            dropdownContent.removeChild(dropdownContent.firstChild);
+        }
+  
+        const options = ['Public', 'Private', 'Online'];
+  
+        const updatedOptions = options.filter(option => option !== selectedText);
+  
+        updatedOptions.forEach(option => {
+            const link = document.createElement('a');
+            link.href = '#';
+            link.textContent = option;
+            dropdownContent.appendChild(link);
+  
+            link.addEventListener('click', function(e) {
+                const selectedText = e.target.textContent;
+                dropbtn.textContent = selectedText;
+                currentSelection = selectedText;
+  
+                updateDropdownList(selectedText);
+            });
+        });
+    }
+  });
